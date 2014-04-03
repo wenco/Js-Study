4be26d32cb308269759bdfe102db1ef95fff9fc7
wenco <wencogo@gmail.com>
2014-04-03 17:30:40 +0800

    Create unique.js

---
+++ unique.js
@@ -0,0 +1,11 @@
+function unique(arr){
+  var result = [], hash = {};
+    for (var i = 0, elem; (elem = arr[i]) != null; i++) {
+        if (!hash[elem]) {
+            result.push(elem);
+            hash[elem] = true;
+        }
+    }
+    //console.log(hash);
+    return result;
+}
